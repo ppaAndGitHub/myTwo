811349362340497c98b9150d7992e7305b0fb282
ppaAndGitHub <1106266541@qq.com>
2019-04-01 10:14:07 +0800

    提交userController

--- src/main/java/com/newtouch/myspringboot/controller/UserController.java
+++ src/main/java/com/newtouch/myspringboot/controller/UserController.java
@@ -5,8 +5,7 @@ import com.newtouch.myspringboot.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -23,5 +22,20 @@ public class UserController {
     public UserVO getUser(){
         log.info("===========开始查询用户========");
         return userService.getUser();
+    }
+
+    /**
+     * 诠释所有接受前台参数的方式
+     * @param userName
+     * @param passwords
+     * @return
+     */
+    @GetMapping("login/{userName}")
+    public UserVO userLogin(@PathVariable(name="userName") String userName , @RequestParam(name="passwords") String passwords){
+        UserVO userVO = new UserVO();
+        userVO.setPasswords(passwords);
+        userVO.setUserName(userName);
+        System.out.println();
+        return new UserVO();
     }
 }
